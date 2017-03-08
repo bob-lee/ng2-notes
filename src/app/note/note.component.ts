3b7819b8a6dc856c13abb2926b46e9bf39966bd1
Bob-PC\Bob <bob.bumsuk.lee@gmail.com>
2017-03-09 07:08:28 +1300

    basic add. remove, edit

--- src/app/note/note.component.ts
+++ src/app/note/note.component.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class NoteComponent implements OnInit {
   @Input() note: any;
   @Output() event = new EventEmitter();
+  noteToEdit: any;
   noteForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) { }
@@ -19,11 +20,12 @@ export class NoteComponent implements OnInit {
       text: ['', Validators.required]
     });
 
-    this.noteForm.controls['name'].setValue(this.note.name);
-    this.noteForm.controls['text'].setValue(this.note.text);
+    // this.noteForm.controls['name'].setValue(this.note.name);
+    // this.noteForm.controls['text'].setValue(this.note.text);
+    this.noteForm.patchValue(this.note);
   }
 
-  save(e) {
+  save(e) { // saving add or edit, todo = 2 or 1
     console.log('save', this.note._id);
     this.note.name = this.noteForm.value.name;
     this.note.text = this.noteForm.value.text;
@@ -31,7 +33,7 @@ export class NoteComponent implements OnInit {
     e.stopPropagation();
   }
 
-  cancel(e) {
+  cancel(e) { // cancelling add or edit, todo = 2 or 1
     this.note.todo = 0;
     this.event.next(this.note);
     e.stopPropagation();
