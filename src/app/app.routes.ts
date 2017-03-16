ea4c2c74511d67ee9cb0e9d6609a5319bce88338
Bob-PC\Bob <bob.bumsuk.lee@gmail.com>
2017-03-17 08:18:35 +1300

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,20 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { OurNotesComponent } from './our-notes/our-notes.component';
-import { GroupComponent } from './group/group.component';
+import { NoteFormComponent } from './note-form/note-form.component';
 
 export const AppRoutes: Routes = [
   { path: '', redirectTo: '/group', pathMatch: 'full' },
-  { path: 'group', component: OurNotesComponent, 
-    /*children: [
-      { path: 'group/:name', component: GroupComponent } 
-    ]*/
+  { path: 'group', component: OurNotesComponent },
+  {
+    path: 'group/:name', component: OurNotesComponent,
+    children: [
+      { path: 'add', component: NoteFormComponent } // 'group/Lee%20family/add'
+    ]
   },
-  { path: 'group/:name', component: OurNotesComponent }
-  // { path: 'group/:name', component: GroupComponent, 
-  //   children: [
-  //     { path: 'add', component: AddNoteComponent },
-  //     { path: 'edit/:id', component: EditNoteComponent } 
-  //   ]
-  // }
+  { path: 'group/:name/edit/:index', component: OurNotesComponent } // 'group/Lee%20family/edit/5'
 ];
